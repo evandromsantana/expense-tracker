ed3ba1f1233b1f88259704f1d10bd8b78c42fef5
EVANDRO DE MACEDO SANTANA <evandromsantana@hotmail.com>
2024-02-22 11:27:01 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+  cartao: { title: "Nubank", color: "blue", expense: true },
+  rent: { title: "Aluguel", color: "brown", expense: true },
+  salary: { title: "Salário", color: "green", expense: false },
+};
---
+++ src/data/items.ts
@@ -0,0 +1,34 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2024, 1, 24),
+    category: "cartao",
+    title: "Nubank",
+    value: 1600.5,
+  },
+  {
+    date: new Date(2024, 1, 25),
+    category: "cartao",
+    title: "Digio",
+    value: 1000.5,
+  },
+  {
+    date: new Date(2024, 1, 30),
+    category: "cartao",
+    title: "Inter",
+    value: 480.5,
+  },
+  {
+    date: new Date(2024, 1, 30),
+    category: "rent",
+    title: "Aluguel Apt",
+    value: 1250,
+  },
+  {
+    date: new Date(2024, 1, 30),
+    category: "salary",
+    title: "Salário",
+    value: 4500,
+  },
+];
